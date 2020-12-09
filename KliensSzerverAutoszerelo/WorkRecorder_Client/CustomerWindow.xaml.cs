ef646114652745c1e024927f44eb3250cd388116
p-adrian05 <adrian16616@gmail.com>
2020-12-09 12:05:13 +0100

    Made CustomerWindow GUI

---
+++ KliensSzerverAutoszerelo/WorkRecorder_Client/CustomerWindow.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WorkRecorder_Client {
+    /// <summary>
+    /// Interaction logic for CustomerWindow.xaml
+    /// </summary>
+    public partial class CustomerWindow : Window {
+        public CustomerWindow() {
+            InitializeComponent();
+        }
+    }
+}
