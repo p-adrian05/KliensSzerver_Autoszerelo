6d56e38de452eaaebce2877228f00a08df63d216
p-adrian05 <adrian16616@gmail.com>
2020-12-13 10:19:49 +0100

    Add GetWorks() function to WorkDataProvider class to fulfill read operation

--- KliensSzerverAutoszerelo/KliensSzerverAutoszerelo_Common/DataProviders/WorkDataProvider.cs
+++ KliensSzerverAutoszerelo/KliensSzerverAutoszerelo_Common/DataProviders/WorkDataProvider.cs
@@ -1,10 +1,29 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Net.Http;
 using System.Text;
+using KliensSzerverAutoszerelo_Common.Models;
+using Newtonsoft.Json;
 
 namespace KliensSzerverAutoszerelo_Common.DataProviders {
     public class WorkDataProvider {
 
         private const String URL = "http://localhost:5000/api/work";
+
+        public static IEnumerable<Work> GetWorks() {
+
+            using (var client = new HttpClient()) {
+
+                var response = client.GetAsync(URL).Result;
+
+                if (response.IsSuccessStatusCode) {
+
+                    var rawData = response.Content.ReadAsStringAsync().Result;
+                    IEnumerable<Work> works = JsonConvert.DeserializeObject<IEnumerable<Work>>(rawData);
+                    return works;
+                }
+                throw new InvalidOperationException(response.StatusCode.ToString());
+            }
+        }
     }
 }
