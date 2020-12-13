ef61efc890223500e6813eaf5d3008af9cdf2251
p-adrian05 <adrian16616@gmail.com>
2020-12-13 10:07:51 +0100

    Init WorkDataProvider class in Common project

---
+++ KliensSzerverAutoszerelo/KliensSzerverAutoszerelo_Common/DataProviders/WorkDataProvider.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace KliensSzerverAutoszerelo_Common.DataProviders {
+    public class WorkDataProvider {
+
+        private const String URL = "http://localhost:5000/api/work";
+    }
+}
