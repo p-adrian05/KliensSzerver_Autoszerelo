7ae3797aec7119c386e95cd535395651f0f80343
p-adrian05 <adrian16616@gmail.com>
2020-12-13 10:27:59 +0100

    Add CreateWork method to WorkDataProvider class to fulfill create operation

--- KliensSzerverAutoszerelo/KliensSzerverAutoszerelo_Common/DataProviders/WorkDataProvider.cs
+++ KliensSzerverAutoszerelo/KliensSzerverAutoszerelo_Common/DataProviders/WorkDataProvider.cs
@@ -8,7 +8,7 @@ using Newtonsoft.Json;
 namespace KliensSzerverAutoszerelo_Common.DataProviders {
     public class WorkDataProvider {
 
-        private const String URL = "http://localhost:5000/api/work";
+        private const string URL = "http://localhost:5000/api/work";
 
         public static IEnumerable<Work> GetWorks() {
 
@@ -22,7 +22,21 @@ namespace KliensSzerverAutoszerelo_Common.DataProviders {
                     IEnumerable<Work> works = JsonConvert.DeserializeObject<IEnumerable<Work>>(rawData);
                     return works;
                 }
-                throw new InvalidOperationException(response.StatusCode.ToString());
+                throw new InvalidOperationException($"Failed to read works {response.StatusCode}");
+            }
+        }
+
+        public static void CreateWork(Work work) {
+
+            using(var client = new HttpClient()) {
+
+                var rawData = JsonConvert.SerializeObject(work);
+                var content = new StringContent(rawData, Encoding.UTF8, "application/json");
+
+                var response = client.PostAsync(URL, content).Result;
+                if (!response.IsSuccessStatusCode) {
+                    throw new InvalidOperationException($"Failed to create work {response.StatusCode}");
+                }
             }
         }
     }
