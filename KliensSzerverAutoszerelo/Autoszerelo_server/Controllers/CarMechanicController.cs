9c049c92a3fba65782f5c281a4602d8c9827d268
BorvizRobi <borviz.robi@gmail.com>
2020-12-19 20:06:19 +0100

    added get post methods in CarMechenicController, also added basic json repository for works

--- KliensSzerverAutoszerelo/Autoszerelo_server/Controllers/CarMechanicController.cs
+++ KliensSzerverAutoszerelo/Autoszerelo_server/Controllers/CarMechanicController.cs
@@ -1,4 +1,5 @@
-﻿using KliensSzerverAutoszerelo_Common.Models;
+﻿using Autoszerelo_Szerver.Repositories;
+using KliensSzerverAutoszerelo_Common.Models;
 using Microsoft.AspNetCore.Mvc;
 using System.Collections.Generic;
 using System.Linq;
@@ -10,22 +11,19 @@ namespace Autoszerelo_Szerver.Controllers
     [ApiController]
     public class CarMechanicController : ControllerBase
     {
-        private Work[] works = new Work[]
-        {
-            new Work{Id=1, FirstName="Gábor", LastName = "Nagy", CarBrand="TOYOTA", CarType="Type", Description="Leírás", LicensePlate="SSS111", WorkState=WorkState.Recorded},
-            new Work{Id=2, FirstName="Sanyi", LastName = "Nagy", CarBrand="TOYOTA", CarType="Type", Description="Leírás", LicensePlate="SSK111", WorkState=WorkState.Recorded}
-        };
-
+       
 
         [HttpGet]
         public ActionResult<IEnumerable<Work>> Get()
         {
+            var works = WorkRepository.GetWorks();
             return Ok(works);
         }
 
         [HttpGet("{id}")]
         public ActionResult<Work> Get(long id)
         {
+            var works = WorkRepository.GetWorks();
             var work = works.FirstOrDefault(x => x.Id == id);
 
             if (work != null)
@@ -37,6 +35,36 @@ namespace Autoszerelo_Szerver.Controllers
                 return NotFound();
             }
             
+        }
+
+        [HttpPost]
+        public ActionResult Post(Work work)
+        {
+            var works = WorkRepository.GetWorks();
+
+            var newId = GetNewId(works);
+            work.Id = newId;
+
+            works.Add(work);
+            WorkRepository.StoreWorks(works);
+
+            return Ok();
+        }
+
+
+        private long GetNewId(IList<Work> works)
+        {
+            long id = 0;
+
+            foreach (var work in works)
+            {
+                if (id < work.Id)
+                {
+                    id = work.Id;
+                }
+            }
+
+            return id + 1;
         }
     }
 }
---
+++ KliensSzerverAutoszerelo/Autoszerelo_server/Repositories/WorkRepository.cs
@@ -0,0 +1,55 @@
+﻿using KliensSzerverAutoszerelo_Common.Models;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace Autoszerelo_Szerver.Repositories
+{
+    public static class WorkRepository
+    {
+        public static IList<Work> GetWorks() 
+        {
+            var appDataPath = GetAppDataPath();
+
+            if (File.Exists(appDataPath))
+            {
+                var rawContent = File.ReadAllText(appDataPath);
+                var works = JsonSerializer.Deserialize<IList<Work>>(rawContent);
+                return works;
+            }
+
+            return new List<Work>();
+        }
+
+        public static void StoreWorks(IList<Work> works)
+        {
+            var appDataPath = GetAppDataPath();
+
+            var rawContent = JsonSerializer.Serialize(works);
+            File.WriteAllText(appDataPath, rawContent);
+        }
+
+        public static string GetAppDataPath()
+        {
+            var localAppFolder = GetLocalFolder();
+            var appDataPath = Path.Combine(localAppFolder,"WorkState.json");
+            return appDataPath;
+        }
+
+        public static string GetLocalFolder()
+        {
+            var localAppDataFolder = Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData);
+            var localAppFolder = Path.Combine(localAppDataFolder, "WebAPI_Server");
+
+            if (!Directory.Exists(localAppFolder))
+            {
+                Directory.CreateDirectory(localAppFolder);
+            }
+
+            return localAppFolder;
+        }
+    }
+}
