eb6a69c9399426496e0aab68a434caa0b0e48ed2
p-adrian05 <adrian16616@gmail.com>
2020-12-20 09:41:21 +0100

    Extend validations define input strings max length

--- KliensSzerverAutoszerelo/WorkRecorder_Client/Validation/CustomerValidation.cs
+++ KliensSzerverAutoszerelo/WorkRecorder_Client/Validation/CustomerValidation.cs
@@ -7,10 +7,18 @@ using System.Text.RegularExpressions;
 
 namespace WorkRecorder_Client.Validation {
     public class CustomerValidation {
+        private const int firstNameLength = 30;
+        private const int lastNameLength = 30;
+        private const int brandLength = 20;
+        private const int carTypeLength = 20;
+
         public static bool ValidateFirstName(String name) {
             if (string.IsNullOrWhiteSpace(name)) {
                 throw new InvalidFirstNameException("FirstName should not be empty.");
-            } else if (!Regex.IsMatch(name, @"^([A-Z][a-z]*)?(\s{0,1}[A-Z][a-z]*)$")) {
+            }else if (name.Length > firstNameLength) {
+                throw new InvalidFirstNameException($"FirstName length is max {firstNameLength} characters");
+            }
+            else if (!Regex.IsMatch(name, @"^([A-Z][a-z]*)?(\s{0,1}[A-Z][a-z]*)$")) {
                 throw new InvalidFirstNameException("Invalid first name");
             }
             return true;
@@ -18,6 +26,8 @@ namespace WorkRecorder_Client.Validation {
         public static bool ValidateLastName(String name) {
             if (string.IsNullOrWhiteSpace(name)) {
                 throw new InvalidLastNameException("Last Name should not be empty.");
+            } else if (name.Length > lastNameLength) {
+                throw new InvalidLastNameException($"LastName length is max {lastNameLength} characters");
             } else if (!Regex.IsMatch(name, @"^[A-Z][a-z]*$")) {
                 throw new InvalidLastNameException("Invalid last name");
             }
@@ -26,6 +36,8 @@ namespace WorkRecorder_Client.Validation {
         public static bool ValidateBrandName(String brandName) {
             if (string.IsNullOrWhiteSpace(brandName)) {
                 throw new InvalidBrandNameException("Brand name should not be empty.");
+            } else if (brandName.Length > brandLength) {
+                throw new InvalidBrandNameException($"Brand length max {brandLength} characters");
             } else if (!Regex.IsMatch(brandName, @"^[A-Z][a-z]*$")) {
                 throw new InvalidBrandNameException("Invalid brand");
             }
@@ -42,6 +54,8 @@ namespace WorkRecorder_Client.Validation {
         public static bool ValidateCarType(String carTypeName) {
             if (string.IsNullOrWhiteSpace(carTypeName)) {
                 throw new InvalidCarTypeException("Car type should not be empty.");
+            } else if (carTypeName.Length > carTypeLength) {
+                throw new InvalidCarTypeException($"Car type length max {carTypeLength} characters");
             } else if (!Regex.IsMatch(carTypeName, @"^(\w*(\s*))*$")) {
                 throw new InvalidCarTypeException("No special characters allowed");
             }
